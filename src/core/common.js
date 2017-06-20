6cf2ce3b476a5df5f5a8be7658fc597da79beb0c
Danny Lin <danny0838@gmail.com>
2017-06-21 01:27:12 +0800

    core: allow "&" in filename

--- src/core/common.js
+++ src/core/common.js
@@ -154,7 +154,7 @@ scrapbook.validateFilename = function (filename, forceAscii) {
   filename = filename
       .replace(/[\x00-\x1F\x7F]+|^ +/g, "")
       .replace(/^\./, "_.").replace(/^ +/, "").replace(/[. ]+$/, "")  // leading/trailing spaces and dots are not allowed in Windows
-      .replace(/[:"?*\\/|&]/g, "_")
+      .replace(/[:"?*\\/|]/g, "_")
       .replace(/[~]/g, "-").replace(/[<]/g, "(").replace(/[>]/g, ")");
   if (forceAscii) {
     filename = filename.replace(/[^\x00-\x7F]+/g, m => encodeURI(m));
