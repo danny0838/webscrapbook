5d0d5375bc52567ab77ccf0bb0f7735a2da46c2d
Danny Lin <danny0838@gmail.com>
2017-07-09 21:10:20 +0800

    options: add export/import functionality

--- src/core/options.js
+++ src/core/options.js
@@ -92,6 +92,60 @@ function setOptionToDocument(id, value) {
   }
 }
 
+function exportOptions() {
+  var data = new Blob([JSON.stringify(scrapbook.options, null, 2)], {type: "application/json"});
+  var elem = document.createElement("a");
+  elem.href = URL.createObjectURL(data);
+  elem.download = "webscrapbook.options." + scrapbook.dateToId().slice(0, 8) + ".json";
+  document.body.appendChild(elem);
+  elem.click();
+  elem.remove();
+}
+
+function importOptions() {
+  document.getElementById("import-input").click();
+}
+
+function importFile(file) {
+  document.getElementById("import-input").value = null;
+
+  let reader = new FileReader();
+  reader.onloadend = function (event) {
+    var text = event.target.result;
+    try {
+      let data = JSON.parse(text);
+      var options = Object.assign(scrapbook.options, data);
+    } catch (ex) {
+      showMessage(scrapbook.lang("ErrorImportOptions", [ex]));
+      return;
+    }
+
+    // import options
+    let remaining = 0;
+    scrapbook.options = options;
+    scrapbook.saveOptions((options) => {
+      if (!chrome.runtime.lastError) {
+        try {
+          for (let id in options) {
+            setOptionToDocument(id, options[id]);
+          }
+          showMessage(scrapbook.lang("OptionsImportSuccess"));
+        } catch (ex) {
+          showMessage(scrapbook.lang("ErrorImportOptions", [ex]));
+        }
+      } else {
+        showMessage(scrapbook.lang("ErrorImportOptions", [chrome.runtime.lastError]));
+      }
+    });
+  }
+  reader.readAsText(file);
+}
+
+function showMessage(msg) {
+  document.getElementById("message").textContent = msg;
+  window.scrollTo(0, 0);
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
   // load languages
   scrapbook.loadLanguages(document);
@@ -105,6 +159,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
       window.close();
     });
     event.preventDefault();
+  });
+
+  document.getElementById("export").addEventListener("click", (event) => {
+    exportOptions();
+    event.preventDefault();
+  });
+
+  document.getElementById("import").addEventListener("click", (event) => {
+    importOptions();
+    event.preventDefault();
+  });
+
+  document.getElementById("import-input").addEventListener("change", (event) => {
+    event.preventDefault();
+    var file = event.target.files[0];
+    importFile(file);
   });
 
   // default options
