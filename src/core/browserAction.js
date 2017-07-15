cde00143b31ac30ace12cca7bc692a5c62f829f3
Danny Lin <danny0838@gmail.com>
2017-07-16 00:22:43 +0800

    ui: improve UI of browserAction in tab (and Firefox Android)

--- src/core/browserAction.js
+++ src/core/browserAction.js
@@ -9,25 +9,106 @@ document.addEventListener('DOMContentLoaded', () => {
   scrapbook.loadLanguages(document);
 
   document.getElementById("captureTab").addEventListener('click', () => {
-    var win = chrome.extension.getBackgroundPage();
-    win.capturer.captureActiveTab();
-    window.close();
+    chrome.tabs.getCurrent((tab) => {
+      if (!tab) {
+        // browserAction.html is a prompt diaglog
+        var win = chrome.extension.getBackgroundPage();
+        win.capturer.captureActiveTab();
+        window.close();
+      } else {
+        // browserAction.html is in a tab (or Firefox Android)
+        let base = document.getElementById("captureTab");
+        let selector = base.nextSibling;
+        if (selector && selector.nodeType === 1) {
+          while (selector.firstChild) { selector.firstChild.remove(); }
+        } else {
+          selector = document.createElement("div");
+          base.parentNode.insertBefore(selector, base.nextSibling);
+        }
+        chrome.tabs.query({
+          windowId: chrome.windows ? chrome.windows.WINDOW_ID_CURRENT : undefined,
+          url: ["http://*/*", "https://*/*", "file://*"]
+        }, (tabs) => {
+          tabs.forEach((tab) => {
+            let elem = document.createElement("div");
+            elem.classList.add("button");
+            elem.classList.add("sub");
+            elem.textContent = (tab.index + 1) + ": " + tab.title;
+            elem.addEventListener('click', (event) => {
+              event.preventDefault;
+              event.stopPropagation;
+              var win = chrome.extension.getBackgroundPage();
+              win.capturer.captureTab(tab);
+              selector.remove();
+            });
+            selector.appendChild(elem);
+          });
+        });
+      }
+    });
   });
 
   document.getElementById("captureTabSource").addEventListener('click', () => {
-    var win = chrome.extension.getBackgroundPage();
-    win.capturer.captureActiveTabSource();
-    window.close();
+    chrome.tabs.getCurrent((tab) => {
+      if (!tab) {
+        // browserAction.html is a prompt diaglog
+        var win = chrome.extension.getBackgroundPage();
+        win.capturer.captureActiveTabSource();
+        window.close();
+      } else {
+        // browserAction.html is in a tab (or Firefox Android)
+        let base = document.getElementById("captureTabSource");
+        let selector = base.nextSibling;
+        if (selector && selector.nodeType === 1) {
+          while (selector.firstChild) { selector.firstChild.remove(); }
+        } else {
+          selector = document.createElement("div");
+          base.parentNode.insertBefore(selector, base.nextSibling);
+        }
+        chrome.tabs.query({
+          windowId: chrome.windows ? chrome.windows.WINDOW_ID_CURRENT : undefined,
+          url: ["http://*/*", "https://*/*", "file://*"]
+        }, (tabs) => {
+          tabs.forEach((tab) => {
+            let elem = document.createElement("div");
+            elem.classList.add("button");
+            elem.classList.add("sub");
+            elem.textContent = (tab.index + 1) + ": " + tab.title;
+            elem.addEventListener('click', (event) => {
+              event.preventDefault;
+              event.stopPropagation;
+              var win = chrome.extension.getBackgroundPage();
+              win.capturer.captureTabSource(tab);
+              selector.remove();
+            });
+            selector.appendChild(elem);
+          });
+        });
+      }
+    });
   });
 
   document.getElementById("captureAllTabs").addEventListener('click', () => {
     var win = chrome.extension.getBackgroundPage();
     win.capturer.captureAllTabs();
-    window.close();
+    chrome.tabs.getCurrent((tab) => {
+      if (!tab) {
+        // browserAction.html is a prompt diaglog
+        window.close();
+      }
+    });
   });
 
   document.getElementById("openOptions").addEventListener('click', () => {
-    window.open(chrome.runtime.getURL("core/options.html"));
-    window.close();
+    chrome.tabs.getCurrent((tab) => {
+      if (!tab) {
+        // browserAction.html is a prompt diaglog
+        window.open(chrome.runtime.getURL("core/options.html"));
+        window.close();
+      } else {
+        // browserAction.html is in a tab (or Firefox Android)
+        document.location = chrome.runtime.getURL("core/options.html");
+      }
+    });
   });
 });
