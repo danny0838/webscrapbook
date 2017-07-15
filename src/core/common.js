b9c7f682792ad0786e3b6b3e88ae12e392110aa2
Danny Lin <danny0838@gmail.com>
2017-07-15 22:50:01 +0800

    core: fix loadLanguages() deletes child nodes

--- src/core/common.js
+++ src/core/common.js
@@ -115,8 +115,7 @@ scrapbook.lang = function (key, args) {
 
 scrapbook.loadLanguages = function (rootNode) {
   Array.prototype.forEach.call(rootNode.getElementsByTagName("*"), (elem) => {
-    var str = elem.textContent;
-    if (/^__MSG_(.*?)__$/.test(str)) {
+    if (elem.childNodes.length === 1 && /^__MSG_(.*?)__$/.test(elem.textContent)) {
       elem.textContent = scrapbook.lang(RegExp.$1);
     }
     Array.prototype.forEach.call(elem.attributes, (attr) => {
