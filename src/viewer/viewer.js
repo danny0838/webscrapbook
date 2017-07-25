danny0838/webarchiveviewer

viewer: pass the zip blob via indexedDB
- This avoids a large ugly dataURI and can pass larger data.

src/viewer.js
@@ -402,11 +402,42 @@ function init() {
       };
 
       var invokeZipViewer = function (zipFile, indexFile, inNewTab) {
-        let onZipRead = function (zipData) {
+        let storeZipData = function (zipFile, callback) {
+          var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+          var request = indexedDB.open("zipFiles", 1); 
+
+          request.onupgradeneeded = function (evt) {
+            var db = evt.target.result;
+            var objectStore = db.createObjectStore("zipFiles", {keyPath: "uuid"});
+          }; 
+
+          request.onsuccess = function (evt) {
+            var uuid = utils.getUuid();
+
+            var db = evt.target.result;
+            var transaction = db.transaction("zipFiles", "readwrite");
+            var objectStore = transaction.objectStore(["zipFiles"]);
+            var req = objectStore.add({uuid: uuid, blob: zipFile});
+
+            transaction.oncomplete = function (evt) {
+              callback(uuid);
+            };
+
+            transaction.onerror = function (evt) {
+              alert("Unable to store the zip file: " + evt.target.error);
+            };
+          };
+           
+          request.onerror = function (evt) {
+            alert("Unable to store the zip file: " + evt.target.error);
+          };
+        };
+
+        let startViewer = function (uuid) {
           let viewerData = {
             virtualBase: chrome.runtime.getURL("!/"),
             indexFile: indexFile,
-            zip: zipData.replace(";", ";filename=" + encodeURIComponent(zipFile.name) + ";"),
+            zipId: uuid,
             // In Firefox, the blob URL page generated by addon is restricted by CSP
             // and inline scripts are not allowed.  Convert them into blob URLs.
             useInlineScriptShim: viewer.isGecko
@@ -501,11 +532,7 @@ function init() {
           }
         };
 
-        let reader = new FileReader();
-        reader.onloadend = function (event) {
-          onZipRead(event.target.result);
-        }
-        reader.readAsDataURL(zipFile);
+        storeZipData(zipFile, startViewer);
       };
 
       var type = utils.filenameParts(zipFile.name)[1].toLowerCase();

danny0838/webscrapbook

src/viewer/viewer.js
@@ -402,11 +402,42 @@ function init() {
       };
 
       var invokeZipViewer = function (zipFile, indexFile, inNewTab) {
-        let onZipRead = function (zipData) {
+        let storeZipData = function (zipFile, callback) {
+          var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+          var request = indexedDB.open("zipFiles", 1); 
+
+          request.onupgradeneeded = function (evt) {
+            var db = evt.target.result;
+            var objectStore = db.createObjectStore("zipFiles", {keyPath: "uuid"});
+          }; 
+
+          request.onsuccess = function (evt) {
+            var uuid = scrapbook.getUuid();
+
+            var db = evt.target.result;
+            var transaction = db.transaction("zipFiles", "readwrite");
+            var objectStore = transaction.objectStore(["zipFiles"]);
+            var req = objectStore.add({uuid: uuid, blob: zipFile});
+
+            transaction.oncomplete = function (evt) {
+              callback(uuid);
+            };
+
+            transaction.onerror = function (evt) {
+              alert("Unable to store the zip file: " + evt.target.error);
+            };
+          };
+           
+          request.onerror = function (evt) {
+            alert("Unable to store the zip file: " + evt.target.error);
+          };
+        };
+
+        let startViewer = function (uuid) {
           let viewerData = {
             virtualBase: chrome.runtime.getURL("viewer/!/"),
             indexFile: indexFile,
-            zip: zipData.replace(";", ";filename=" + encodeURIComponent(zipFile.name) + ";"),
+            zipId: uuid,
             // In Firefox, the blob URL page generated by addon is restricted by CSP
             // and inline scripts are not allowed.  Convert them into blob URLs.
             useInlineScriptShim: viewer.isGecko
@@ -501,11 +532,7 @@ function init() {
           }
         };
 
-        let reader = new FileReader();
-        reader.onloadend = function (event) {
-          onZipRead(event.target.result);
-        }
-        reader.readAsDataURL(zipFile);
+        storeZipData(zipFile, startViewer);
       };
 
       var type = scrapbook.filenameParts(zipFile.name)[1].toLowerCase();