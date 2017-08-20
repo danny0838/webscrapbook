be4c4c11ba0394a47a0da47d134c5fd8d5a3ed12
Danny Lin <danny0838@gmail.com>
2017-08-21 01:18:56 +0800

    core: support browserAction for Firefox Android >= 55

--- src/core/pageActionLoader.js
+++ src/core/pageActionLoader.js
@@ -6,7 +6,16 @@
  *******************************************************************/
 
 if (!chrome.browserAction) {
+  // Firefox Android < 55: no browserAction
+  // Fallback to pageAction.
   // Firefox Android ignores the tabId parameter and
   // shows the pageAction for all tabs
   chrome.pageAction.show(0);
+} else if (!chrome.browserAction.getPopup) {
+  // Firefox Android >= 55: only browserAction onClick
+  // Open the browserAction page
+  chrome.browserAction.onClicked.addListener((tab) => {
+    var url = chrome.runtime.getURL("core/browserAction.html");
+    chrome.tabs.create({url: url, active: true}, () => {});
+  });
 }
