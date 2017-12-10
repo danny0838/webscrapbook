21bee8f44d3553de18e0d4a8701fab05db4a6c6f
Danny Lin <danny0838@gmail.com>
2017-12-10 22:56:52 +0800

    core: fix dataUriToFile when filename defined
    - If the data URL has filename parameter and useFilename is false, a bad extension is got.

--- src/core/common.js
+++ src/core/common.js
@@ -1020,7 +1020,7 @@ scrapbook.dataUriToFile = function (dataUri, useFilename = true) {
     if (useFilename && parameters.filename) {
       filename = decodeURIComponent(parameters.filename);
     } else {
-      let ext = parameters.filename || Mime.prototype.extension(mime);
+      let ext = parameters.filename && scrapbook.filenameParts(parameters.filename)[1] || Mime.prototype.extension(mime);
       ext = ext ? ("." + ext) : "";
       filename = scrapbook.sha1(ab, "ARRAYBUFFER") + ext;
     }
