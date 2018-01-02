9ea4904518a4227a12a313eddd2425ddaf50493c
Danny Lin <danny0838@gmail.com>
2018-01-02 22:42:04 +0800

    indexer: extend server.py
    - Allow customizing protocol.
    - Support HTTPS.
    - Optimize code.

--- src/resources/server.py
+++ src/resources/server.py
@@ -2,13 +2,14 @@
 import os
 import configparser
 import webbrowser
-import http.server
 import re
 import zipfile
 import time
 
 import urllib.parse
 from http import HTTPStatus
+from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+import ssl
 
 # init global variables
 config = {
@@ -16,11 +17,15 @@ config = {
     "port": "8080",
     "bind": "127.0.0.1",
     "host": "localhost",
+    "protocol": "HTTP/1.1",
+    "ssl_on": "false",
+    "ssl_key": "",
+    "ssl_cert": "",
     "browse": "true",
     "entry": "tree/frame.html"
     }
 
-class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+class HTTPRequestHandler(SimpleHTTPRequestHandler):
 
     def parse_zip_time_tuple(self, tuple):
         date_time = '{}/{}/{} {}:{}:{}'.format(*tuple)
@@ -125,12 +130,11 @@ class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             f.close()
             raise
 
-    http.server.BaseHTTPRequestHandler.protocol_version = "HTTP/1.1"
-    http.server.SimpleHTTPRequestHandler.extensions_map[".md"] = "text/markdown"
-    http.server.SimpleHTTPRequestHandler.extensions_map[".htz"] = "application/html+zip"
-    http.server.SimpleHTTPRequestHandler.extensions_map[".maff"] = "application/x-maff"
+    SimpleHTTPRequestHandler.extensions_map[".md"] = "text/markdown"
+    SimpleHTTPRequestHandler.extensions_map[".htz"] = "application/html+zip"
+    SimpleHTTPRequestHandler.extensions_map[".maff"] = "application/x-maff"
 
-def loadServerConfig(path, encoding='UTF-8', **kwargs):
+def load_server_config(path, encoding='UTF-8', **kwargs):
     if not os.path.isfile(path): return
     cp = configparser.ConfigParser()
     cp.read(path, encoding, **kwargs)
@@ -138,24 +142,57 @@ def loadServerConfig(path, encoding='UTF-8', **kwargs):
     for key in c:
         config[key] = c[key]
 
+def start_server(HandlerClass=HTTPRequestHandler,
+            ServerClass=HTTPServer,
+            protocol="HTTP/1.0", host='localhost', port=8000, bind="",
+            ssl_on=False, ssl_key=None, ssl_cert=None):
+    HandlerClass.protocol_version = protocol
+
+    httpd = ServerClass((bind, port), HandlerClass)
+
+    if ssl_on:
+        httpd.socket = ssl.wrap_socket(httpd.socket,
+                ssl_key, ssl_cert, server_side=True)
+
+    msg = "Serving {protocol} for {bind} at {scheme}://{host}:{port} ..."
+    msg = msg.format(protocol=protocol,
+            scheme='https' if ssl_on else 'http',
+            host=host, port=port, bind=bind)
+    print(msg)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, stopping server.")
+
 def main():
     # load config
-    loadServerConfig('config.ini')
+    load_server_config('config.ini')
 
     # switch to the specified directory
     os.chdir(config['root'])
 
+    ssl_on = config['ssl_on'].lower() in ['1', 'on', 'yes', 'true']
+    port = int(config['port'])
+
     # start the browser if "browse" is set to a string path
     if config['browse'].lower() in ['1', 'on', 'yes', 'true']:
-        webbrowser.open('http://{host}{port}{path}'.format(
-            host=config['host'],
-            port=':' + config['port'] if config['port'] != '80' else '',
-            path='/' + config['entry'] if config['entry'] else ''
-            ))
+        webbrowser.open('{scheme}://{host}{port}{path}'.format(
+                scheme='https' if ssl_on else 'http',
+                host=config['host'],
+                port='' if (not ssl_on and port == 80) or (ssl_on and port == 443)
+                        else ':' + str(port),
+                path='/' + config['entry'] if config['entry'] else ''
+                ))
 
     # start server
-    http.server.test(HandlerClass=HTTPRequestHandler,
-                     port=int(config['port']), bind=config['bind'])
+    start_server(protocol=config['protocol'],
+            host=config['host'],
+            port=port,
+            bind=config['bind'],
+            ssl_on=ssl_on,
+            ssl_key=config['ssl_key'],
+            ssl_cert=config['ssl_cert'])
 
 if __name__ == '__main__':
     main()
