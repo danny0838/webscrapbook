ef27dfb690ec60be43d13da5e112d4a653fbc04a
Danny Lin <danny0838@gmail.com>
2018-01-02 23:16:21 +0800

    test: optimize server.py

--- test/server.py
+++ test/server.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import http.server
-import webbrowser
-import json
 import os
-import urllib
+import json
+import http.server
 
 class HTTPRequestHandler(http.server.CGIHTTPRequestHandler):
     """
