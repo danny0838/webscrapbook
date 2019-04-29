eb06e6d50fef6c2fc0b53f713c522a4f9a340300
Danny Lin <danny0838@gmail.com>
2019-04-29 18:10:27 +0800

    indexer: reword code of generated pages

--- src/indexer/load.js
+++ src/indexer/load.js
@@ -2612,7 +2612,7 @@ const conf = {
   scrapbooks: [
     {name: "", path: "${scrapbook.getRelativeUrl('', this.treeDir)}", dataDir: "${this.dataDir}", treeDir: "${this.treeDir}"}
   ],
-  allowHttp: 0,  // whether to load rdf cache from the http? -1: deny, 0: ask; 1: allow
+  allowHttp: 0,  // whether to load js cache from HTTP(S)? -1: deny, 0: ask; 1: allow
   defaultSearch: "-type:folder -type:separator",  // the constant string to add before the input keyword
   defaultField: "tcc",  // the field to search for bare key terms
   viewInMapPath: "${scrapbook.escapeQuotes(this.indexPage)}",  // path (related to treeDir) of the map page for "view in map"
