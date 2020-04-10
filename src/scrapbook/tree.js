cc3cca5428c97e4831f859e8b447fc0d3cad92cd
Danny Lin <danny0838@gmail.com>
2020-04-10 14:36:25 +0800

    scrapbook: forbid book switch when UI disabled

--- src/scrapbook/tree.js
+++ src/scrapbook/tree.js
@@ -795,6 +795,7 @@ Redirecting to file <a href="index.md">index.md</a>
     },
 
     enableUi(willEnable) {
+      document.getElementById('book').disabled = !willEnable;
       document.getElementById('command').disabled = !willEnable;
     },
 
