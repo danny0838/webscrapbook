728282636eb586701f2c2cb03227f9de26c8f1fe
Danny Lin <danny0838@gmail.com>
2020-04-17 14:57:05 +0800

    test: add test for a CSS from HTTP Link header

---
+++ test/t/capture_css_header_link/header-css.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A quick test for a CSS loaded via HTTP Link header.
+
+- Supported by Firefox (76.*) but not accessible via CSSOM.
+- Not supported by Chromium (80.*).
+"""
+import sys
+sys.stdout.buffer.write("""Content-Type: text/html
+Link: <header.css>; rel="stylesheet"
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<blockquote id="header">header background: yellow;</blockquote>
+</body>
+</html>
+""".encode("UTF-8"))
