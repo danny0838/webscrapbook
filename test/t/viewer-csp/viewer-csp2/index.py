a142f7ba19a945b4842e0b7d721c9a9fd60e2497
Danny Lin <danny0838@gmail.com>
2021-11-15 20:51:39 +0800

    viewer: block blob: for embed, object, applet for security

--- test/t/viewer-csp/viewer-csp2/index.py
+++ test/t/viewer-csp/viewer-csp2/index.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""An alternative entry to test the index.html with dynamic port
+"""
 import sys
 import os
 import json
@@ -6,7 +8,7 @@ import json
 port = json.loads(os.environ['wsb.config'])['server_port2']
 port = '' if port == 80 else ':' + str(port)
 sys.stdout.buffer.write("""Content-Type: text/html
-Content-Disposition: inline; filename="inexe.html"
+Content-Disposition: inline; filename="index.html"
 
 """.encode("ASCII"))
 with open(os.path.join(os.path.dirname(__file__), "index.html"), "r") as fh:
