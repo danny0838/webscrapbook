a1bb1e19af8c47ed07394ab92edda887d6f99f6d
Danny Lin <danny0838@gmail.com>
2023-06-01 23:20:27 +0800

    core: upgrade browser-polyfill.js to v0.10.0

--- src/lib/browser-polyfill.js
+++ src/lib/browser-polyfill.js
@@ -11,7 +11,7 @@
     global.browser = mod.exports;
   }
 })(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function (module) {
-  /* webextension-polyfill - v0.8.0 - Tue Apr 20 2021 11:27:38 */
+  /* webextension-polyfill - v0.10.0 - Fri Aug 12 2022 19:42:44 */
 
   /* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
 
@@ -22,9 +22,12 @@
    * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
   "use strict";
 
-  if (typeof browser === "undefined" || Object.getPrototypeOf(browser) !== Object.prototype) {
-    const CHROME_SEND_MESSAGE_CALLBACK_NO_RESPONSE_MESSAGE = "The message port closed before a response was received.";
-    const SEND_RESPONSE_DEPRECATION_WARNING = "Returning a Promise is the preferred way to send a reply from an onMessage/onMessageExternal listener, as the sendResponse will be removed from the specs (See https://developer.mozilla.org/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage)"; // Wrapping the bulk of this polyfill in a one-time-use function is a minor
+  if (!globalThis.chrome?.runtime?.id) {
+    throw new Error("This script should only be loaded in a browser extension.");
+  }
+
+  if (typeof globalThis.browser === "undefined" || Object.getPrototypeOf(globalThis.browser) !== Object.prototype) {
+    const CHROME_SEND_MESSAGE_CALLBACK_NO_RESPONSE_MESSAGE = "The message port closed before a response was received."; // Wrapping the bulk of this polyfill in a one-time-use function is a minor
     // optimization for Firefox. Since Spidermonkey does not fully parse the
     // contents of a function until the first time it's called, and since it will
     // never actually need to be called, this allows the polyfill to be included
@@ -1074,9 +1077,7 @@
           });
           listener(wrappedReq);
         };
-      }); // Keep track if the deprecation warning has been logged at least once.
-
-      let loggedSendResponseDeprecationWarning = false;
+      });
       const onMessageWrappers = new DefaultWeakMap(listener => {
         if (typeof listener !== "function") {
           return listener;
@@ -1105,11 +1106,6 @@
           let wrappedSendResponse;
           let sendResponsePromise = new Promise(resolve => {
             wrappedSendResponse = function (response) {
-              if (!loggedSendResponseDeprecationWarning) {
-                console.warn(SEND_RESPONSE_DEPRECATION_WARNING, new Error().stack);
-                loggedSendResponseDeprecationWarning = true;
-              }
-
               didCallSendResponse = true;
               resolve(response);
             };
@@ -1261,17 +1257,13 @@
         }
       };
       return wrapObject(extensionAPIs, staticWrappers, apiMetadata);
-    };
-
-    if (typeof chrome != "object" || !chrome || !chrome.runtime || !chrome.runtime.id) {
-      throw new Error("This script should only be loaded in a browser extension.");
-    } // The build process adds a UMD wrapper around this file, which makes the
+    }; // The build process adds a UMD wrapper around this file, which makes the
     // `module` variable available.
 
 
     module.exports = wrapAPIs(chrome);
   } else {
-    module.exports = browser;
+    module.exports = globalThis.browser;
   }
 });
 //# sourceMappingURL=browser-polyfill.js.map
