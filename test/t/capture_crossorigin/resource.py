b6c08abaa9bfecb7014eb263b80c89193b17a9b6
Danny Lin <danny0838@gmail.com>
2023-09-16 23:50:48 +0800

    test: fix a potential script error

--- test/t/capture_crossorigin/resource.py
+++ test/t/capture_crossorigin/resource.py
@@ -10,7 +10,7 @@ q = parse_qs(os.environ['QUERY_STRING'])
 res = next(iter(q.get('res')), None)
 file = os.path.join(os.path.dirname(script), 'resource', res)
 mime, _ = mimetypes.guess_type(file)
-mime = mime or application/octet-stream
+mime = mime or 'application/octet-stream'
 
 sys.stdout.buffer.write(f"""Content-Type: {mime}
 Access-Control-Allow-Origin: *
