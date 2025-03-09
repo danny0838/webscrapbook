1611a2bf13fb500bf573529c49bfc673b8e505fe
Danny Lin <danny0838@gmail.com>
2025-03-09 21:00:22 +0800

    core: support manifest v2/v3

--- src/capturer/background.js
+++ src/capturer/background.js
@@ -214,13 +214,13 @@
         count = matchTypeAndCount.session;
       }
 
-      browser.browserAction.setBadgeText({
+      browser.action.setBadgeText({
         tabId,
         text: count.toString(),
       });
 
       // For a set with tabId, badge color will be reset when the tab is navigated
-      browser.browserAction.setBadgeBackgroundColor({
+      browser.action.setBadgeBackgroundColor({
         tabId,
         color,
       });
