14e38f5affcd8c02a37ef24e62bc2985640a6def
Danny Lin <danny0838@gmail.com>
2025-03-17 09:17:21 +0800

    core: remove compatibility code for old browsers

--- src/core/common.js
+++ src/core/common.js
@@ -1572,7 +1572,6 @@ if (typeof Promise.withResolvers === 'undefined') {
     }
 
     // create a modal dialog with mask
-    const supportsDialog = typeof HTMLDialogElement !== 'undefined';
 
     // in case there is another existing prompt due to inproper trigger
     for (const elem of doc.documentElement.querySelectorAll('[data-scrapbook-elem="toolbar-prompt"]')) {
@@ -1595,10 +1594,6 @@ dialog {
   position: fixed;
   inset: 0;
 }
-.mask {
-  z-index: 2147483647;
-  background: rgba(0, 0, 0, 0.4);
-}
 `;
 
     const dialog = shadow.appendChild(doc.createElement('dialog'));
@@ -1612,12 +1607,8 @@ dialog {
     });
     observer.observe(doc.documentElement, {childList: true});
 
-    if (supportsDialog) {
-      dialog.addEventListener('close', () => reject(new Error('dialog closed')));
-      dialog.showModal();
-    } else {
-      dialog.classList.add('mask');
-    }
+    dialog.addEventListener('close', () => reject(new Error('dialog closed')));
+    dialog.showModal();
 
     const id = scrapbook.getUuid();
 
@@ -2164,12 +2155,6 @@ dialog {
     try {
       yield* docOrShadowRoot.adoptedStyleSheets;
     } catch (ex) {
-      // Firefox < 101.0b1: docOrShadowRoot.adoptedStyleSheets is undefined
-      //
-      // Firefox < 101.0b8: docOrShadowRoot.adoptedStyleSheets of a content
-      // script throws an error when accessed.
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1767819
-      //
       // Firefox >= 101.0b8: docOrShadowRoot.adoptedStyleSheets of a content
       // script has all properties unreadable.
       // https://bugzilla.mozilla.org/show_bug.cgi?id=1770592
@@ -4693,28 +4678,11 @@ dialog {
     if (scrapbook.userAgent.is('gecko')) {
       let updateData = {};
 
-      // Firefox < 86: `focused` in `createData` causes an error.
       // Firefox >= 86: ignores `focused: false` in `createData`.
       // https://bugzilla.mozilla.org/show_bug.cgi?id=1213484
       // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/create#browser_compatibility
       if (typeof createData.focused !== 'undefined') {
         updateData.focused = createData.focused;
-        if (scrapbook.userAgent.major < 86) {
-          delete createData.focused;
-        }
-      }
-
-      // Firefox < 109: ignores `left` and `top` in `createData` for popups.
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1271047
-      if (scrapbook.userAgent.major < 109 && createData.type === 'popup') {
-        if (typeof createData.left !== 'undefined') {
-          updateData.left = createData.left;
-          delete createData.left;
-        }
-        if (typeof createData.top !== 'undefined') {
-          updateData.top = createData.top;
-          delete createData.top;
-        }
       }
 
       if (Object.keys(updateData).length) {
