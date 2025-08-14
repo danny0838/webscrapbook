c2f2d1e8a0238afed0f2f603520708c5897c7e48
Danny Lin <danny0838@gmail.com>
2025-08-14 21:53:02 +0800

    scrapbook: allow customizing debug level

--- src/scrapbook/check.js
+++ src/scrapbook/check.js
@@ -16,6 +16,8 @@
 
   'use strict';
 
+  const LOGGING_LEVELS = {debug: 1, info: 2, warn: 3, error: 4, critical: 5};
+
   let logger;
 
   function log(info) {
@@ -31,11 +33,23 @@
     logger = document.getElementById('logger');
     logger.textContent = '';
 
+    const params = new URLSearchParams(new URL(document.URL).search);
+
+    const logLevel = LOGGING_LEVELS[params.get('debug')] || 2;
+    const css = Object.entries(LOGGING_LEVELS).reduce((css, [name, level]) => {
+      if (level < logLevel) {
+        css.push(`.${name} { display: none; }`);
+      }
+      return css;
+    }, []);
+    const elem = document.head.appendChild(document.createElement('style'));
+    elem.textContent = css.join('\n');
+
     try {
       await server.init();
 
       // handle URL actions
-      const query = new URLSearchParams(new URL(document.URL).search);
+      const query = params;
       query.set('a', 'check');
 
       return await server.requestSse({
