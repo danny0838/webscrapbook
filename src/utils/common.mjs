d357dd70248a21f3c0281d25ad2b626a743e863c
Danny Lin <danny0838@gmail.com>
2025-10-01 20:29:19 +0800

    core: modularize `doc-cloner.mjs`

--- src/utils/common.mjs
+++ src/utils/common.mjs
@@ -482,8 +482,6 @@ const HTTP_STATUS_TEXT = {
   511: "Network Authentication Required",
 };
 
-const DOMPARSER_SUPPORT_TYPES = new Set(['text/html', 'application/xhtml+xml', 'text/xml', 'application/xml', 'image/svg+xml']);
-
 const SCRAPBOOK_OBJECT_REMOVE_TYPE_REMOVE = new Set(["annotation", "freenote", "sticky", "block-comment", "custom"]);
 const SCRAPBOOK_OBJECT_REMOVE_TYPE_UNWRAP = new Set(["linemarker", "inline", "link-url", "link-inner", "link-file", "custom-wrapper"]);
 const SCRAPBOOK_OBJECT_REMOVE_TYPE_UNCOMMENT = new Set(["erased"]);
@@ -1424,112 +1422,6 @@ function* getAdoptedStyleSheets(docOrShadowRoot) {
     }
   }
 }
-
-/**
- * Clone a document and generate relation mapping.
- *
- * @param {Document} doc
- * @param {Object} [options]
- * @param {Map|WeakMap} [options.origNodeMap]
- * @param {Map|WeakMap} [options.clonedNodeMap]
- * @return {Document} The cloned document.
- */
-function cloneDocument(doc, {
-  origNodeMap,
-  clonedNodeMap,
-} = {}) {
-  const {contentType: mime, documentElement: docElemNode} = doc;
-  const newDoc = (new DOMParser()).parseFromString(
-    '<' + docElemNode.nodeName.toLowerCase() + '/>',
-    DOMPARSER_SUPPORT_TYPES.has(mime) ? mime : 'text/html',
-  );
-  while (newDoc.firstChild) {
-    newDoc.removeChild(newDoc.firstChild);
-  }
-  origNodeMap?.set(newDoc, doc);
-  clonedNodeMap?.set(doc, newDoc);
-  return newDoc;
-}
-
-/**
- * Clone a node and generate relation mapping.
- *
- * @param {Node} node
- * @param {boolean} [deep]
- * @param {Object} [options]
- * @param {Document} [options.newDoc]
- * @param {Map|WeakMap} [options.origNodeMap]
- * @param {Map|WeakMap} [options.clonedNodeMap]
- * @param {boolean} [options.includeShadowDom]
- * @return {Node} The cloned node.
- */
-const cloneNode = (() => {
-  const cloneShadowDom = (node, newNode, options = {}) => {
-    const shadowRoot = getShadowRoot(node);
-    if (!shadowRoot) { return; }
-    const {origNodeMap, clonedNodeMap, includeShadowDom} = options;
-    let newShadowRoot = getShadowRoot(newNode);
-    if (newShadowRoot) {
-      // shadowRoot already cloned (when shadowRoot.clonable = true)
-      // map the shadowRoot and descendant nodes
-      const walker1 = shadowRoot.ownerDocument.createNodeIterator(shadowRoot);
-      const walker2 = newShadowRoot.ownerDocument.createNodeIterator(newShadowRoot);
-      let node1 = walker1.nextNode();
-      let node2 = walker2.nextNode();
-      while (node1) {
-        origNodeMap?.set(node2, node1);
-        clonedNodeMap?.set(node1, node2);
-        includeShadowDom && cloneShadowDom(node1, node2, options);
-        node1 = walker1.nextNode();
-        node2 = walker2.nextNode();
-      }
-    } else {
-      newShadowRoot = newNode.attachShadow({
-        mode: shadowRoot.mode,
-        clonable: shadowRoot.clonable,
-        delegatesFocus: shadowRoot.delegatesFocus,
-        serializable: shadowRoot.serializable,
-        slotAssignment: shadowRoot.slotAssignment,
-      });
-      origNodeMap?.set(newShadowRoot, shadowRoot);
-      clonedNodeMap?.set(shadowRoot, newShadowRoot);
-      for (const node of shadowRoot.childNodes) {
-        newShadowRoot.appendChild(cloneNode(node, true, options));
-      }
-    }
-  };
-
-  return function cloneNode(node, deep = false, options = {}) {
-    const {
-      newDoc = node.ownerDocument,
-      origNodeMap,
-      clonedNodeMap,
-      includeShadowDom,
-    } = options;
-
-    const newNode = newDoc.importNode(node, deep);
-
-    if (deep) {
-      const walker1 = node.ownerDocument.createNodeIterator(node);
-      const walker2 = newDoc.createNodeIterator(newNode);
-      let node1 = walker1.nextNode();
-      let node2 = walker2.nextNode();
-      while (node1) {
-        origNodeMap?.set(node2, node1);
-        clonedNodeMap?.set(node1, node2);
-        includeShadowDom && cloneShadowDom(node1, node2, options);
-        node1 = walker1.nextNode();
-        node2 = walker2.nextNode();
-      }
-    } else {
-      origNodeMap?.set(newNode, node);
-      clonedNodeMap?.set(node, newNode);
-      includeShadowDom && cloneShadowDom(node, newNode, options);
-    }
-
-    return newNode;
-  };
-})();
 
 /**
  * Convert dynamic information into representable HTML attributes for an
@@ -3935,8 +3827,6 @@ export {
   getScrapBookObjectElems,
   getShadowRoot,
   getAdoptedStyleSheets,
-  cloneDocument,
-  cloneNode,
   htmlifyElem,
   htmlify,
   unhtmlifyElem,
