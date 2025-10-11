60d861a08f306c96f6ea193808eaa9d680504d9c
Danny Lin <danny0838@gmail.com>
2025-10-11 18:42:24 +0800

    capture: rework remove mode for `rewriteSvgHref`
    - No more remove the node directly.

--- src/test/test_capturer_doc-handler.mjs
+++ src/test/test_capturer_doc-handler.mjs
@@ -8458,7 +8458,8 @@ Default3\
 
                       break;
                     }
-                    case "blank": {
+                    case "blank":
+                    case "remove": {
                       it('should remove the attribute', async function () {
                         var doc = docFactory({attrs: [[`${prefix}href`, './myicon.bmp', ns]]});
 
@@ -8468,20 +8469,6 @@ Default3\
 
                         sinon.assert.calledOnceWithExactly(spyResolveLink, './myicon.bmp', 'https://example.com/');
                         sinon.assert.calledWithExactly(spyRewrite, elem, `${prefix}href`, null);
-                      });
-
-                      break;
-                    }
-                    case "remove": {
-                      it('should remove the element', async function () {
-                        var doc = docFactory({attrs: [[`${prefix}href`, './myicon.bmp', ns]]});
-                        var elemOrig = doc.querySelector(tagName);
-
-                        var rewriter = await new TestCapturer().captureDocument({doc, docUrl, options});
-                        assert.isNull(rewriter.doc.querySelector(tagName));
-
-                        sinon.assert.calledOnceWithExactly(spyResolveLink, './myicon.bmp', 'https://example.com/');
-                        sinon.assert.calledOnceWithExactly(spyRemove, rewriter.getClonedNode(elemOrig));
                       });
 
                       break;
