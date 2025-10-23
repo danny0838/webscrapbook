3707b9a08b382bb9edda9170a38ba76e456e912e
Danny Lin <danny0838@gmail.com>
2025-10-23 10:53:35 +0800

    capture: use canonical referrer value when a legacy value is provided

--- src/test/test_capturer_doc-handler.mjs
+++ src/test/test_capturer_doc-handler.mjs
@@ -2796,7 +2796,7 @@ Default3\
               }
             });
 
-            $it.xfail()('should update document referrer policy to a canonical value when the value is legacy', async function () {
+            it('should update document referrer policy to a canonical value when the value is legacy', async function () {
               for (const [value, newValue] of META_REFERRER_POLICY_LEGACY) {
                 var doc = createDocFixture({tagName, attrs: {name: 'referrer', content: value}});
 
