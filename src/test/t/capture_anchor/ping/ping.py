2d402e1246cb3a72528ff3ccc8b28f2d043a10b2
Danny Lin <danny0838@gmail.com>
2025-11-02 16:36:42 +0800

    test: remove e2e tests covered by unit tests

--- src/test/t/capture_anchor/ping/ping.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-sys.stdout.buffer.write("""Content-Type: text/plain;charset=UTF-8
-Cache-Control: no-store
-
-""".encode('ASCII'))
--- src/test/t/capture_header_mime/noext.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-sys.stdout.buffer.write(b"""Content-Type: application/msword
-Content-Disposition: inline; filename="noext"
-
-""")
