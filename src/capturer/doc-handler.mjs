1ba50fe131861c3ad2bda49f29b08afa6920c8e6
Danny Lin <danny0838@gmail.com>
2025-11-21 18:48:09 +0800

    capture: honor only direct children for `picture`/`audio`/`video`

--- src/capturer/doc-handler.mjs
+++ src/capturer/doc-handler.mjs
@@ -2811,7 +2811,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
   [`_handle_{${NS_HTML}}picture`](elem) {
     const {isHeadless, refUrl, tasks, settings, options} = this;
 
-    for (const subElem of elem.querySelectorAll('source[srcset]')) {
+    for (const subElem of elem.querySelectorAll(':scope > source[srcset]')) {
       if (subElem.namespaceURI !== NS_HTML) { continue; }
       const rewriteSrcset = utils.rewriteSrcset(subElem.getAttribute("srcset"), (url) => {
         return this.resolveRelativeUrl(url, this.baseUrl);
@@ -2819,14 +2819,14 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
       this.captureRewriteAttr(subElem, "srcset", rewriteSrcset);
     }
 
-    const [imgElem] = Array.prototype.filter.call(elem.querySelectorAll('img'), x => x.namespaceURI === NS_HTML);
+    const [imgElem] = Array.prototype.filter.call(elem.querySelectorAll(':scope > img'), x => x.namespaceURI === NS_HTML);
     const refPolicy = imgElem?.referrerPolicy || this.docRefPolicy;
     switch (options["capture.image"]) {
       case "link":
         // do nothing
         break;
       case "blank":
-        for (const subElem of elem.querySelectorAll('source[srcset]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[srcset]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           this.captureRewriteAttr(subElem, "srcset", null);
         }
@@ -2836,7 +2836,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
         throw new NodeDisconnect(elem);
       case "save-current":
         if (!isHeadless) {
-          for (const subElem of elem.querySelectorAll('img')) {
+          for (const subElem of elem.querySelectorAll(':scope > img')) {
             if (subElem.namespaceURI !== NS_HTML) { continue; }
             const subElemOrig = this.getOrigNode(subElem);
 
@@ -2847,7 +2847,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
             }
           }
 
-          for (const subElem of elem.querySelectorAll('source[srcset]')) {
+          for (const subElem of elem.querySelectorAll(':scope > source[srcset]')) {
             if (subElem.namespaceURI !== NS_HTML) { continue; }
             this.captureRemoveNode(subElem);
           }
@@ -2858,7 +2858,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
         // eslint-disable-next-line no-fallthrough
       case "save":
       default: {
-        for (const subElem of elem.querySelectorAll('source[srcset]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[srcset]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           tasks.push(async () => {
             const response = await utils.rewriteSrcset(subElem.getAttribute("srcset"), async (url) => {
@@ -2888,7 +2888,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
       this.captureRewriteAttr(elem, "src", newUrl);
     }
 
-    for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+    for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
       if (subElem.namespaceURI !== NS_HTML) { continue; }
       const newUrl = this.resolveRelativeUrl(subElem.getAttribute("src"), this.baseUrl);
       this.captureRewriteAttr(subElem, "src", newUrl);
@@ -2905,7 +2905,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
 
         // HTML 5.1 2nd Edition / W3C Recommendation:
         // The src attribute must be present and be a valid non-empty URL.
-        for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           this.captureRewriteAttr(subElem, "src", "about:blank");
         }
@@ -2919,7 +2919,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
           const elemOrig = this.getOrigNode(elem);
           if (elemOrig?.currentSrc) {
             const url = elemOrig.currentSrc;
-            for (const subElem of elem.querySelectorAll('source[src]')) {
+            for (const subElem of elem.querySelectorAll(':scope > source[src]')) {
               if (subElem.namespaceURI !== NS_HTML) { continue; }
               this.captureRemoveNode(subElem);
             }
@@ -2936,7 +2936,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
             });
           }
 
-          for (const subElem of elem.querySelectorAll('track[src]')) {
+          for (const subElem of elem.querySelectorAll(':scope > track[src]')) {
             if (subElem.namespaceURI !== NS_HTML) { continue; }
             tasks.push(async () => {
               const response = await this.downloadFile({
@@ -2973,7 +2973,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
           });
         }
 
-        for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           tasks.push(async () => {
             const response = await this.downloadFile({
@@ -3007,7 +3007,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
       this.captureRewriteAttr(elem, "src", newUrl);
     }
 
-    for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+    for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
       if (subElem.namespaceURI !== NS_HTML) { continue; }
       const newUrl = this.resolveRelativeUrl(subElem.getAttribute("src"), this.baseUrl);
       this.captureRewriteAttr(subElem, "src", newUrl);
@@ -3030,7 +3030,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
 
         // HTML 5.1 2nd Edition / W3C Recommendation:
         // The src attribute must be present and be a valid non-empty URL.
-        for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           this.captureRewriteAttr(subElem, "src", "about:blank");
         }
@@ -3058,7 +3058,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
           const elemOrig = this.getOrigNode(elem);
           if (elemOrig?.currentSrc) {
             const url = elemOrig.currentSrc;
-            for (const subElem of elem.querySelectorAll('source[src]')) {
+            for (const subElem of elem.querySelectorAll(':scope > source[src]')) {
               if (subElem.namespaceURI !== NS_HTML) { continue; }
               this.captureRemoveNode(subElem);
             }
@@ -3075,7 +3075,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
             });
           }
 
-          for (const subElem of elem.querySelectorAll('track[src]')) {
+          for (const subElem of elem.querySelectorAll(':scope > track[src]')) {
             if (subElem.namespaceURI !== NS_HTML) { continue; }
             tasks.push(async () => {
               const response = await this.downloadFile({
@@ -3126,7 +3126,7 @@ class CaptureDocumentRewriter extends MapperMixin(CaptureDocumentRewriterBase) {
           });
         }
 
-        for (const subElem of elem.querySelectorAll('source[src], track[src]')) {
+        for (const subElem of elem.querySelectorAll(':scope > source[src], :scope > track[src]')) {
           if (subElem.namespaceURI !== NS_HTML) { continue; }
           tasks.push(async () => {
             const response = await this.downloadFile({
